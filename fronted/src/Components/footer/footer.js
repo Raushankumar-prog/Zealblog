fe8e322a0d1a68a1c556524a9eddb0e39e307d79
Raushan kumar <raushankumar8757825@gmail.com>
2023-11-24 19:02:33 +0530

    writng footer

--- fronted/src/Components/footer/footer.js
+++ fronted/src/Components/footer/footer.js
@@ -4,7 +4,23 @@ import './footer.css';
 const Footer = () => {
     return ( <div className="footer">
             <div className="size">
-                34fsff
+              <p id="line">Zealblog is platform where author can post the article.</p> 
+              <hr></hr>
+          
+            <div className="action">
+                <div className="first">
+                    <div className="a1">Author</div>
+                    <div className="a1">cookies policy</div>
+                    <div className="a1">web notifications</div>
+                </div>
+                <div className="second">
+                     <div className="a">careers</div>
+                    <div className="a">terms & conditions</div></div>
+                <div className="third">
+                       <div className="a">topics</div>
+                    <div className="a">advertise with us</div>
+                </div>
+                  </div>
             </div>
     </div> );
 }
