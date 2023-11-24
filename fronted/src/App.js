c252b96332c9b56a0c7d551a9012597cd09ab3de
Raushan kumar <raushankumar8757825@gmail.com>
2023-11-24 18:06:38 +0530

    making the footer of pade

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import Footer from './Components/footer/footer';
 function App() {
   return (
     <div className="hu">
      <p>initiling the project.
      </p>
+     <Footer/>
     </div>
   );
 }
--- fronted/src/Components/footer/footer.js
+++ fronted/src/Components/footer/footer.js
@@ -1,1 +1,12 @@
 import './footer.css';
+
+
+const Footer = () => {
+    return ( <div className="footer">
+            <div className="size">
+                34fsff
+            </div>
+    </div> );
+}
+ 
+export default Footer;
