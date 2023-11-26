91ac6e37d8329302fb1951aace06fb0de6b7e12d
Raushan kumar <raushankumar8757825@gmail.com>
2023-11-26 11:32:13 +0530

    designing the briefing

--- fronted/src/Components/Glance/glance.js
+++ fronted/src/Components/Glance/glance.js
@@ -1,6 +1,24 @@
+import './glance.css';
+import img from './OIP.jpeg';
 const Glance = () => {
     return ( 
-              <div className="main"> fnehfbhdbvjbdfjbvjfdnjkbnjknfdkjbnjkfdnjkbdcfevc</div>
+              <div className="main"> 
+                            
+                   <div className="universe">
+                            <div className="space">
+                                   <div className="headingglance"><p className="headingtext">Nasa is launching a new rocket for it Artimis mission.</p></div>
+                                  <div className="glancecontent">
+                                        <div className="glanceimage"><img src={img} ></img></div>
+                                         <div className="glancebriefbox"><p className="glancebrieftext"> Artimis is plan of Nasa to land on MOON in 2024.<br></br>They are making rcoket which csn essily take them on moon.</p></div>
+                                   </div>
+                            </div>
+                            <div className="little">
+                                     <div className="author">cfvf</div>
+                                     <div className="like"> cc</div>
+                                     <div className="comment"> c</div>
+                            </div>
+                   </div>
+              </div>
     );
 }
  
