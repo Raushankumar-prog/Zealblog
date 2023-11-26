d3a4ab61e592c5a5f1e67b40b8529e4b83581053
Raushan kumar <raushankumar8757825@gmail.com>
2023-11-26 13:15:13 +0530

    making the base for glance

--- fronted/src/Components/Glance/glance.js
+++ fronted/src/Components/Glance/glance.js
@@ -1,8 +1,11 @@
 import './glance.css';
 import img from './OIP.jpeg';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import CommentIcon from '@mui/icons-material/Comment';
 const Glance = () => {
     return ( 
-              <div className="main"> 
+              <div className="main1"> 
                             
                    <div className="universe">
                             <div className="space">
@@ -13,11 +16,29 @@ const Glance = () => {
                                    </div>
                             </div>
                             <div className="little">
-                                     <div className="author">cfvf</div>
-                                     <div className="like"> cc</div>
-                                     <div className="comment"> c</div>
+                                     <div className="author"> <AccountCircleIcon fontSize="small"/>profile
+                           </div>
+                                     <div className="like"><ThumbUpOffAltIcon/></div>
+                                     <div className="comment"><CommentIcon/></div>
                             </div>
                    </div>
+                    <div className="universe">
+                            <div className="space">
+                                   <div className="headingglance"><p className="headingtext">Nasa is launching a new rocket for it Artimis mission.</p></div>
+                                  <div className="glancecontent">
+                                        <div className="glanceimage"><img src={img} ></img></div>
+                                         <div className="glancebriefbox"><p className="glancebrieftext"> Artimis is plan of Nasa to land on MOON in 2024.<br></br>They are making rcoket which csn essily take them on moon.</p></div>
+                                   </div>
+                            </div>
+                            <div className="little">
+                                     <div className="author"> <AccountCircleIcon fontSize="small"/>profile
+                           </div>
+                                     <div className="like"><ThumbUpOffAltIcon/></div>
+                                     <div className="comment"><CommentIcon/></div>
+                            </div>
+                   </div>
+                   
+                   
               </div>
     );
 }
