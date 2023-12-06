bbcdd91751472de05cd8798eec8e14db2d1a5216
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-06 20:28:42 +0530

    writing function for creating post and deleting post

---
+++ backend/src/handlers/business.ts
@@ -0,0 +1,26 @@
+
+
+export const createPost=async(req,res)=>{
+    const post=await prisma.Post.create({
+        data:{
+           title:req.Post.title,
+           content:req.Post.content,
+           nichetype:req.Post.nichetype
+    }
+    })
+}
+
+
+export const publish=async(req,res)=>{
+    const publishpost=await prisma.Post.update({
+        data:{
+           published:req.Post.published
+        }
+    })
+}
+
+const deletedPost = await prisma.Post.delete({
+      where: {
+        id: id
+      },
+    })
