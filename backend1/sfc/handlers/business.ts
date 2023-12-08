67d1fa5980cdc8faea0114154922e17b51f5b1bc
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-08 14:27:33 +0530

    creating a function named deletePOst

--- backend1/sfc/handlers/business.ts
+++ backend1/sfc/handlers/business.ts
@@ -1,26 +1,31 @@
 import prisma from '../db'
 
-export const createPost=async(req,res)=>{
-    const post=await prisma.Post.create({
-        data:{
-           title:req.Post.title,
-           content:req.Post.content,
-           nichetype:req.Post.nichetype
-    }
-    })
-}
 
-
-export const publish=async(req,res)=>{
-    const publishpost=await prisma.Post.update({
-        data:{
-           published:req.Post.published
+export const createPost = async (req, res) => {
+    const post = await prisma.Post.create({
+        data: {
+            title: req.body.title,
+            content: req.body.content,
+            nichetype: req.body.nichetype
         }
-    })
-}
+    });
+};
 
-const deletedPost = await prisma.Post.delete({
-      where: {
-        id: id
-      },
-    })
+export const publish = async (req, res) => {
+    const publishpost = await prisma.Post.update({
+        data: {
+            published: req.body.published
+        }
+    });
+};
+
+
+export const deletePost = async (req, res) => {
+    const id = req.params.id; // Assuming you get the post id from the request parameters
+    const deletedPost = await prisma.Post.delete({
+        where: {
+            id: parseInt(id) // Assuming your id is an integer, adjust accordingly
+        }
+    });
+    res.send("Post deleted successfully");
+};
