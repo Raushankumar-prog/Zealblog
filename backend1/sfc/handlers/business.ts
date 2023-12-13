89df3bcb13c5a628e3a676f7464b9c2d3a8b185a
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-13 06:44:01 +0530

    defining the update functiom

--- backend1/sfc/handlers/business.ts
+++ backend1/sfc/handlers/business.ts
@@ -1,8 +1,8 @@
 import prisma from '../db'
 
-
+//creating the post
 export const createPost = async (req, res) => {
-    const post = await prisma.Post.create({
+    const post = await prisma.post.create({
         data: {
             title: req.body.title,
             content: req.body.content,
@@ -11,20 +11,40 @@ export const createPost = async (req, res) => {
     });
 };
 
+// updating the post
+export const updatePost=async(req,res)=>{
+    const updatepost=await prisma.post.update({
+       where:{
+        id:req.body.id
+       },
+       data:{
+        title:req.body.title,
+        content:req.body.content,
+        nichetype:req.body.nichetype
+       }
+     } );
+
+    
+}
+//updating the publish timing
+
 export const publish = async (req, res) => {
-    const publishpost = await prisma.Post.update({
+    const publishpost = await prisma.post.update({
+        where:{
+               id:req.body.id
+        },
         data: {
             published: req.body.published
         }
     });
 };
 
-
+//deleting the post
 export const deletePost = async (req, res) => {
     const id = req.params.id; // Assuming you get the post id from the request parameters
-    const deletedPost = await prisma.Post.delete({
+    const deletedPost = await prisma.post.delete({
         where: {
-            id: parseInt(id) // Assuming your id is an integer, adjust accordingly
+            id: id
         }
     });
     res.send("Post deleted successfully");
