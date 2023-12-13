651b2546c8e09cb7864d334dc6dd493a3cc00d52
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-13 11:17:53 +0530

    adding the comment handling file

--- backend1/sfc/handlers/comment.ts
+++ backend1/sfc/handlers/comment.ts
@@ -1,17 +1,14 @@
-import prisma from "../db";
-
+import prisma from '../db';
 
 // creating the comment
-export const createCommnet = async (req, res) => {
+export const createComment = async (req, res) => {
     try {
-        const post = await prisma.comment.create({
+        const comment = await prisma.comment.create({
             data: {
-               
-            content: req.body.content,
-        
+                content: req.body.content
             }
         });
-        res.status(200).json({ success: true, post });
+        res.status(200).json({ success: true, comment });
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
@@ -19,19 +16,17 @@ export const createCommnet = async (req, res) => {
 };
 
 // updating the comment
-export const updatePost = async (req, res) => {
+export const updateComment = async (req, res) => {
     try {
-        const updatedPost = await prisma.comment.update({
+        const updatedComment = await prisma.comment.update({
             where: {
                 id: req.body.id
             },
             data: {
-            
-                content: req.body.content,
-                
+               content: req.body.content
             }
         });
-        res.status(200).json({ success: true, updatedPost });
+        res.status(200).json({ success: true, updatedComment });
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
@@ -39,15 +34,15 @@ export const updatePost = async (req, res) => {
 };
 
 // deleting the comment
-export const deletePost = async (req, res) => {
+export const deleteComment = async (req, res) => {
     try {
         const id = req.params.id;
-        const deletedPost = await prisma.comment.delete({
+        const deletedComment = await prisma.comment.delete({
             where: {
                 id: id
             }
         });
-        res.status(200).json({ success: true, message: 'comment deleted successfully' });
+        res.status(200).json({ success: true, message: 'Comment deleted successfully' });
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
