8219fe85bc335443692c00c3c596348552acb7dd
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-18 13:44:32 +0530

    using fetch to connet the backend and fronted for taking user

--- fronted/src/Components/left/leftcomponent/profile/profile.js
+++ fronted/src/Components/left/leftcomponent/profile/profile.js
@@ -1,10 +1,62 @@
+import React, { useState } from 'react';
 import './profile.css';
 
+const str = "http://localhost:4001/user";
 
 const Profile = () => {
-    return ( <div>
-               
-    </div> );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getdata = () => {
+    const url = new URL(str);
+    const req = new Request(url, {
+      headers: { 'x-steve': 'hello' },
+      method: 'POST',
+      cache: 'reload',
+      body: JSON.stringify({
+        'username': username,
+        'password': password
+      })
+    });
+
+    fetch(req)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Invalid");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        // Handle the data received from the server
+        console.log(data);
+      })
+      .catch((err) => {
+        console.warn(err.message);
+      });
+  }
+
+  return (
+    <div>
+      <form>
+        <label>username:</label>
+        <input
+          type="text"
+          placeholder='username'
+          id='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>password:</label>
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='button' onClick={getdata}>submit</button>
+      </form>
+    </div>
+  );
 }
- 
+
 export default Profile;
