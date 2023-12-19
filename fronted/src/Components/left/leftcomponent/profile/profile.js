747b2963c71be8954743f75addd7ee5b4257e3b2
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-19 12:50:07 +0530

    onclick profile , it open signin or person profile

--- fronted/src/Components/left/leftcomponent/profile/profile.js
+++ fronted/src/Components/left/leftcomponent/profile/profile.js
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './profile.css';
 import Cookies from 'js-cookie';
 
-const str = "http://localhost:4001/user";
+const apiUrl = "http://localhost:4001/user";
 
 const Profile = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  let user={
-     username:username,
-     password:password
+  const [checkusername, setCheckUsername] = useState(false);
+
+  const user = {
+    username: username,
+    password: password
   };
 
   const getdata = () => {
-    const url = new URL(str);
+    const url = new URL(apiUrl);
     const req = new Request(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  
-  },
- 
-  body: JSON.stringify(user),
-});
-
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(user),
+    });
 
     fetch(req)
       .then((res) => {
@@ -34,50 +33,52 @@ const Profile = () => {
       })
       .then((data) => {
         const token = data.token;
-        const usernam=data.user.username;
-        const id=data.user.id;
+        const usernam = data.user.username;
+        const id = data.user.id;
         Cookies.set('token', token, { expires: 7, secure: true });
-         Cookies.set('usernam',usernam,{ expires: 7, secure: true });
-         Cookies.set('id',id,{expires:7,secure:true});
+        Cookies.set('usernam', usernam, { expires: 7, secure: true });
+        Cookies.set('id', id, { expires: 7, secure: true });
+        setCheckUsername(true);
       })
       .catch((err) => {
         console.warn(err.message);
       });
   }
 
-const checkusername=Cookies.get('usernam');
-
-if(checkusername){
-  const form=document.getElementById("form");
-  form.style.display='none';
-}else{
-      const content=document.getElementById("content");
-      content.style.display='block';
-}
+  useEffect(() => {
+    // Check if the username is present in cookies
+    const checkUsername = Cookies.get('usernam');
+    if (checkUsername) {
+      setCheckUsername(true);
+    }
+  }, []);
 
   return (
     <div>
-      <div id="form">
-      <form>
-        <label>username:</label>
-        <input
-          type="text"
-          placeholder='username'
-          id='username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label>password:</label>
-        <input
-          type='password'
-          placeholder='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type='button' onClick={getdata}>submit</button>
-      </form>
-      </div>
-      <div id="content" >hdjd</div>
+      {checkusername ? (
+        <div id="content">if the person is login in , then show this profile</div>
+      ) : (
+        <div id="form">
+          <form>
+            <label>username:</label>
+            <input
+              type="text"
+              placeholder='username'
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>password:</label>
+            <input
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type='button' onClick={getdata}>submit</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
