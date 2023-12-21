6f87dfb8a107f7af9cc560fc09036317859266d2
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-21 10:31:42 +0530

    making of author post template

--- fronted/src/Components/channel_template/channel_component/authorpost/authorpost.js
+++ fronted/src/Components/channel_template/channel_component/authorpost/authorpost.js
@@ -5,8 +5,19 @@ import img from '../../../Glance/OIP.jpeg';
 
 const Authorpost = () => {
     return ( <div className="authorposting">
-                    
-                              
+                            <div className='posted'>
+                               
+                                   <div className='postimage'><img src={img}/></div>
+                                    <div className="postnichenamebox"> <span className='postnichename'>Space News</span></div> 
+                                   <div className='postheadingbox'><span className="postheading">Moon mission lead by billionaire Raushan kumar is about to complete</span></div>
+                                    <div className='authorbox'>
+                                            
+                                                <span className='authorheading'>raushan kumar</span>
+                                                <div  className='authorheadingline'><hr className="vertical" color="black"/></div>
+                                                <span  className='authorheading'>1-2-2022</span>
+                                    </div>
+                            </div>
+                      <div className='postline'><hr className="vertical1"/> </div>       
     </div> );
 }
  
