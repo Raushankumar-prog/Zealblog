188e97ece2fa2f3b51f2c1cbf1636a60c199e634
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-21 14:41:02 +0530

    adding notification api

--- fronted/src/Components/left/leftcomponent/notification/notification.js
+++ fronted/src/Components/left/leftcomponent/notification/notification.js
@@ -1,10 +1,33 @@
 import './notification.css';
+import React from 'react';
 
+const NotificationButton = () => {
+  const showNotification = () => {
+    if (Notification.permission === 'granted') {
+      new Notification('Hello, World!');
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          new Notification('Hello, World!');
+        }
+      });
+    }
+  };
+
+  return (
+    <button onClick={showNotification}>
+      Show Notification
+    </button>
+  );
+};
 
 const Notification = () => {
-    return ( <div>
-         notification
-    </div> );
-}
- 
+  return (
+    <div>
+      <h1>Notification Example</h1>
+      <NotificationButton />
+    </div>
+  );
+};
+
 export default Notification;
