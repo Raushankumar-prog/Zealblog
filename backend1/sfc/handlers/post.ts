6f52a1c018ca030053b9ad5c68d93f3320e281c5
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-24 07:24:07 +0530

    writing function for the lastest post

--- backend1/sfc/handlers/post.ts
+++ backend1/sfc/handlers/post.ts
@@ -8,16 +8,15 @@ export const createPost = async (req, res) => {
                 title: req.body.title,
                 content: req.body.content,
                 nichetype: req.body.nichetype,
-                 belongsid: req.body.id,
-              
-            
+                belongsid: req.body.id,
             }
         });
         res.status(200).json({ success: true, post });
-
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
+    } finally {
+        await prisma.$disconnect();
     }
 };
 
@@ -25,36 +24,35 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     try {
         const updatedPost = await prisma.post.update({
-            
             where: {
-                id:req.body.id,
+                id: req.body.id,
             },
             data: {
-                title:req.body.title,
+                title: req.body.title,
                 content: req.body.content,
-                nichetype:req.body.nichetype,
-                 belongsid:req.body.belongsid,
+                nichetype: req.body.nichetype,
+                belongsid: req.body.belongsid,
             }
         });
         res.status(200).json({ success: true, updatedPost });
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
+    } finally {
+        await prisma.$disconnect();
     }
 };
 
 // updating the publish timing
 export const publish = async (req, res) => {
     try {
-       
-
         const publishedPost = await prisma.post.update({
             where: {
-                id:req.body.id,
+                id: req.body.id,
             },
             data: {
-                published:req.body.publish,
-                belongsid:req.body.belongsid,
+                published: req.body.publish,
+                belongsid: req.body.belongsid,
             }
         });
         console.log('Published Post:', publishedPost);
@@ -62,22 +60,44 @@ export const publish = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
+    } finally {
+        await prisma.$disconnect();
     }
 };
 
 // deleting the post
 export const deletePost = async (req, res) => {
     try {
-         //const {id}=req.params
-        const deletedpost= await prisma.post.delete({
+        const deletedpost = await prisma.post.delete({
             where: {
-                id:req.body.id,
+                id: req.body.id,
             },
-        })
-            
-        res.status(200).json({ success: true, deletedpost});
+        });
+        res.status(200).json({ success: true, deletedpost });
     } catch (error) {
         console.error(error);
         res.status(404).json({ success: false, error: 'Internal Server Error' });
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+// fetching the latest posts
+export const latestPost = async (req, res) => {
+    try {
+        const latestPosts = await prisma.post.findMany({
+            where: {
+                id: req.body.id,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+        res.status(200).json({ latestPosts });
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({ success: false, error: 'Internal Server Error' });
+    } finally {
+        await prisma.$disconnect();
     }
 };
--- backend1/sfc/modules/auth.ts
+++ backend1/sfc/modules/auth.ts
@@ -49,3 +49,4 @@ export const protect = (req, res, next) => {
     return;
   }
 };
+
