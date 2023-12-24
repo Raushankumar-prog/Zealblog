7d575abc6027a3f4424b02bc8683a8a3c2a06703
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-24 08:15:55 +0530

    setting up routes for different function

--- backend1/sfc/router.ts
+++ backend1/sfc/router.ts
@@ -1,21 +1,22 @@
 import {Router} from 'express'
 import { createComment, deleteComment, updateComment } from './handlers/comment'
-import { createPost, deletePost, publish, updatePost } from './handlers/post';
-import { deleteuser } from './handlers/user';
+import { createPost, deletePost, latestPost, popularPosts, publish, updatePost } from './handlers/post';
+import { deleteuser, getSavedPosts } from './handlers/user';
 
 const router = Router()
 
 
- router.post('/createcomment',createComment);
- router.delete('/deletecomment',deleteComment);
+router.post('/createcomment',createComment);
+router.delete('/deletecomment',deleteComment);
 router.put('/updatecomment',updateComment);
- router.post('/createpost',createPost);
+router.post('/createpost',createPost);
 router.delete('/deletepost',deletePost);
 router.put('/updatepost',updatePost);
 router.put('/publish',publish);
-
+router.get('/popularpost',popularPosts);
+router.get('/lastestpost',latestPost);
+router.get('savedpost',getSavedPosts);
 router.delete('/deleteuser',deleteuser);
-
 
 
 
