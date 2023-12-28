9f7ff722d06a8b4b9bfb1efd8a94556a31cc8be5
Raushan kumar <raushankumar8757825@gmail.com>
2023-12-28 17:39:47 +0530

    making funtion to lastest function

--- fronted/src/Components/left/leftcomponent/lastest/lastest.js
+++ fronted/src/Components/left/leftcomponent/lastest/lastest.js
@@ -1,7 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import './lastest.css';
+import { makeRequest } from '../../../fetch/fetch';
 
 const Lastest = () => {
-    return (<div> lastest</div> );
-}
- 
+  const [latestPosts, setLatestPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Make a request to fetch the latest posts
+        const response = await makeRequest('/lastestpost', 'GET', {
+          id: '',
+        });
+
+    
+        setLatestPosts(response.latestPosts);
+      } catch (error) {
+        console.error('Error fetching latest posts:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  return (
+    <div>
+      <h2>Latest Posts</h2>
+      {latestPosts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          {/* Render other post details */}
+          <img src={post.imageUrl} alt="Post Image" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Lastest;
