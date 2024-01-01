b8868a0f71bc5a26af0370360c9ea7406b3e671e
Raushan kumar <raushankumar8757825@gmail.com>
2024-01-01 10:08:47 +0530

    writing getauthor function for sorting author via their number of post

---
+++ backend1/sfc/handlers/author.ts
@@ -0,0 +1,39 @@
+import prisma from '../db';
+
+
+export const getAuthors = async (req, res) => {
+  try {
+    const authorsWithPostCount = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        post: {
+          select: {
+            id: true,
+          },
+        },
+      },
+      orderBy: {
+        post: {
+          _count: 'desc',
+        },
+      },
+      where: {
+        post: {
+          some: {
+            id: {
+              not: null,
+            },
+          },
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, authorsWithPostCount });
+  } catch (error) {
+    console.error('Error fetching authors:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- backend1/sfc/router.ts
+++ backend1/sfc/router.ts
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import { createComment, deleteComment, updateComment } from './handlers/comment'
 import { createPost, deletePost, latestPost, popularPosts, publish, updatePost } from './handlers/post';
 import { deleteuser, getSavedPosts } from './handlers/user';
+import { getAuthors } from './handlers/author';
 
 const router = Router()
 
@@ -14,6 +15,7 @@ router.delete('/deletepost',deletePost);
 router.patch('/updatepost',updatePost);
 router.put('/publish',publish);
 router.get('/popularpost',popularPosts);
+router.get('/getauthors',getAuthors);
 router.get('/lastestpost',latestPost);
 router.get('/savedpost', getSavedPosts);
 router.delete('/deleteuser',deleteuser);
