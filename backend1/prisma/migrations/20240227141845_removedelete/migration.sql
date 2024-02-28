59a284f6b86825776bfff9b710869ee8e7569280
Raushan kumar <raushankumar8757825@gmail.com>
2024-02-28 11:45:50 +0530

     enhancing prisma but not worked

---
+++ backend1/prisma/migrations/20240227141845_removedelete/migration.sql
@@ -0,0 +1,41 @@
+-- DropForeignKey
+ALTER TABLE "comment" DROP CONSTRAINT "comment_belongsid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "liked" DROP CONSTRAINT "liked_belongsid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "liked" DROP CONSTRAINT "liked_postid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "post" DROP CONSTRAINT "post_belongsid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "saving" DROP CONSTRAINT "saving_belongsid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "saving" DROP CONSTRAINT "saving_postid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "subscribe" DROP CONSTRAINT "subscribe_belongsid_fkey";
+
+-- AddForeignKey
+ALTER TABLE "post" ADD CONSTRAINT "post_belongsid_fkey" FOREIGN KEY ("belongsid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_belongsid_fkey" FOREIGN KEY ("belongsid") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "saving" ADD CONSTRAINT "saving_postid_fkey" FOREIGN KEY ("postid") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "saving" ADD CONSTRAINT "saving_belongsid_fkey" FOREIGN KEY ("belongsid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscribe" ADD CONSTRAINT "subscribe_belongsid_fkey" FOREIGN KEY ("belongsid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "liked" ADD CONSTRAINT "liked_postid_fkey" FOREIGN KEY ("postid") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "liked" ADD CONSTRAINT "liked_belongsid_fkey" FOREIGN KEY ("belongsid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
