70b9568a0199a6467fc71c4485d578cb77fd6b1e
Raushan kumar <raushankumar8757825@gmail.com>
2024-07-13 12:48:52 +0530

    change port

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
+const port = process.env.PORT ||4001;
 
 import app from './server'
 
-app.listen(4001, () => {
+app.listen(port, () => {
   console.log('hello on http://localhost:4001')
 })
